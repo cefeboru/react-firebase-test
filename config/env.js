9951323805fffc6dfe4b4aec0528d75af496ffe2
Cesar Bonilla <cefeboru@gmail.com>
2019-06-13 08:58:27 -0600

    Using firebase as a module

--- config/env.js
+++ config/env.js
@@ -77,6 +77,14 @@ function getClientEnvironment(publicUrl) {
         // This should only be used as an escape hatch. Normally you would put
         // images into the `src` and `import` them in code to get their paths.
         PUBLIC_URL: publicUrl,
+        // Firebase config
+        FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+        FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
+        FIREBASE_DATABASE_URL: process.env.FIREBASE_DATABASE_URL,
+        FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+        FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
+        FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
+        FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
       }
     );
   // Stringify all values so we can feed into Webpack DefinePlugin
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -6,6 +6,13 @@ declare namespace NodeJS {
   interface ProcessEnv {
     readonly NODE_ENV: 'development' | 'production' | 'test';
     readonly PUBLIC_URL: string;
+    readonly FIREBASE_API_KEY: string;
+    readonly FIREBASE_AUTH_DOMAIN: string;
+    readonly FIREBASE_DATABASE_URL: string;
+    readonly FIREBASE_PROJECT_ID: string;
+    readonly FIREBASE_STORAGE_BUCKET: string;
+    readonly FIREBASE_MESSAGING_SENDER_ID: string;
+    readonly FIREBASE_APP_ID: string;
   }
 }
 
