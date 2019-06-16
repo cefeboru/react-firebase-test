c30584109008e3769922bd1e7f86641fb434ff59
Cesar Bonilla <cefeboru@gmail.com>
2019-06-15 21:41:46 -0600

    Create initial youtube service

--- src/modules/YoutubeService.ts
+++ src/modules/YoutubeService.ts
@@ -1,6 +1,24 @@
+import Axios from 'axios';
+
 export class YoutubeService {
     /**
      *
      */
-  constructor() {}
+  apiBase: string = 'https://www.googleapis.com/youtube/v3';
+  searchUrl: string = `${this.apiBase}/search`;
+  apiKey: string;
+  constructor() {
+    this.apiKey = process.env.FIREBASE_API_KEY;
+  }
+
+  async searchVideos(query: string, maxResults = 15) {
+    const response = await Axios.get(`${this.searchUrl}?maxResults=${maxResults}&&part=id,snippet&type=video&key=${this.apiKey}&q=${query}`);
+    return response.data;
+  }
 }
+export interface SearchVideosResponse {
+  items: any[];
+  nextPageToken: string;
+}
+
+export default new YoutubeService();
